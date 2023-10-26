437fd8b2d01f0c8eaf0189e6cadefb7398d2c230
Kelvin Gao <u6511753@au.edu>
2023-10-26 12:48:54 +0630

    Initial Commit

---
+++ PodCast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodCast
+//
+//  Created by Kelvin Gao  on 26/10/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
